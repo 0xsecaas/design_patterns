badafe0e791adb4cb995a6784a224ec5e2be0c87
Charly <github.womankind045@passinbox.com>
2025-07-21 17:03:13 +0330

    refactor(creational): add method

--- src/creational/factory/mod.rs
+++ src/creational/factory/mod.rs
@@ -1,2 +1,5 @@
+/// Create entire families of related objects without tying them to specific implementations.
 mod abstract_pattern;
+
+/// Enhances object creation with flexibility and extensibility
 mod method;
